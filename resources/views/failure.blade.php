eec4174ee8e461d1afe97a286c1a915458d1ffd2
Hache_raw <djinn.vsl@gmail.com>
2024-03-27 21:07:55 +0000

    Payment error messages

--- resources/views/failure.blade.php
+++ resources/views/failure.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.notice')
 
 @section('content')
-  <h1>{{ __('An error occurred') }}</h1>
-  <p><strong>{{ __('Bank response')  }}: {{ $payment->response_message }}</strong></p>
+@if (!empty($payment->response_message))
+  <h1>{{ __('Payment failed') }}</h1>
+  <p><strong>{{ __('Bank response') }}: {{ $payment->response_message }}</strong></p>
+  @else
+  <p><strong>{{ __('Did you cancel the process?') }}</strong></p>
+  @endif
   <p>{{ __('Don\'t hesitate to contact us if you have any questions')  }}</p>
 @endsection
