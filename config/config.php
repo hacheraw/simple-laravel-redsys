43cc65b36a953d93ef9718cc2faa2ea1df3f0620
Hache_raw <djinn.vsl@gmail.com>
2024-04-08 11:20:58 +0100

    Admin Panel Allowed emails

--- config/config.php
+++ config/config.php
@@ -8,5 +8,8 @@ return [
   'design' => [
     'logo' => env('APP_LOGO', 'https://placehold.co/400x120?text=Simple+Laravel+Redsys'),
     'background' => env('BACKGROUND_IMAGE', 'https://placehold.co/1920x1080')
+  ],
+  'panel' => [
+    'allowed_emails' => explode(',', env('PANEL_ALLOWED_EMAILS', 'admin@example.com'))
   ]
 ];
